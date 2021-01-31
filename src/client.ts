e23294d3f96528d739447f7a17e1874ec95d8337
Jordan Jones <jordpjones@gmail.com>
2021-01-30 18:53:54 -0800

    Boop

--- src/client.ts
+++ src/client.ts
@@ -1,12 +1,14 @@
 import { Client } from 'mcproto'
 
 export interface ClientOptions {
+  host?: string
+  port?: number
   accessToken?: string
   profile?: string
-      /** @default 120000 ms */
-      timeout?: number
-      /** @default 10000 ms */
-      connectTimeout?: number
+  /** @default 120000 ms */
+  timeout?: number
+  /** @default 10000 ms */
+  connectTimeout?: number
 }
 
 const defaultOptions: Partial<ClientOptions> = {
@@ -18,5 +20,17 @@ export class RedstoneClient extends Client {
   public constructor(options: ClientOptions = {}) {
     super(options);
 
+  }
+
+  /**
+   * Connects to the remote server.
+   * @param {string} [host] Hostname of the server you want to connect to.
+   * @param {number} [port] Port of the server you want to connect to.
+   */
+  async connect(host?: string, port?: number | null) {
+
+    const connection = await super.connect(host, port)
+
+    return connection;
   }
 }
